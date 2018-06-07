77ee042bcb13f98370bf1c623ec55d03fad7f910
Administrator <penglong0705@126.com>
2018-06-07 17:00:53 +0800

    修改spi项目

--- soa-other-dubbo-spi/src/main/java/com/soa/other/spi/service/impl/ShutdownCommand.java
+++ soa-other-dubbo-spi/src/main/java/com/soa/other/spi/service/impl/ShutdownCommand.java
@@ -1,9 +1,11 @@
-package com.soa.other.spi;
+package com.soa.other.spi.service.impl;
+
+import com.soa.other.spi.service.Command;
 
 /**
  * Created by pengyunlong on 2018/5/25.
  */
-public class ShutdownCommand  implements Command{
+public class ShutdownCommand  implements Command {
     @Override
     public void excute() {
         System.out.println("shutdown...");
